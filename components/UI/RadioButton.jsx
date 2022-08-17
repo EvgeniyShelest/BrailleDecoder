9ab1194d1252e7f2d8be60ed171d48b70be8c258
Evgeniy Shelest <zhck12@gmail.com>
2022-08-17 13:56:29 +0300

    Restyle RadioButton

--- components/UI/RadioButton.jsx
+++ components/UI/RadioButton.jsx
@@ -3,10 +3,11 @@ import { View } from 'react-native';
 export default function RadioButton(props) {
   return (
       <View style={[{
-        height: 24,
-        width: 24,
-        borderRadius: 12,
-        borderWidth: 2,
+        height: 48,
+        width: 48,
+        margin: 10,
+        borderRadius: 24,
+        borderWidth: 3,
         borderColor: '#000',
         alignItems: 'center',
         justifyContent: 'center',
@@ -14,9 +15,9 @@ export default function RadioButton(props) {
         {
           props.selected ?
             <View style={{
-              height: 12,
-              width: 12,
-              borderRadius: 6,
+              height: 24,
+              width: 24,
+              borderRadius: 12,
               backgroundColor: '#000',
             }}/>
             : null
